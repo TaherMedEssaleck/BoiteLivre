0d6e029bf153f975f320f2c3338d4f0ec6442e5e
cherif <22014@esp.mr>
2024-10-30 04:32:57 +0100

    adding handling errors in boite service

--- polytech/src/main/java/com/capgemini/polytech/services/BoiteService.java
+++ polytech/src/main/java/com/capgemini/polytech/services/BoiteService.java
@@ -3,6 +3,8 @@ package com.capgemini.polytech.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 import com.capgemini.polytech.dto.BoiteDto;
 import com.capgemini.polytech.mappers.BoiteMapper;
@@ -19,15 +21,17 @@ public class BoiteService {
     @Autowired
     private BoiteMapper boiteMapper;
 
-
     public List<BoiteDto> getAllBoites() {
         return boiteMapper.toDTO(boiteRepository.findAll());
     }
 
 
     public BoiteDto getBoiteById(Integer id) {
-    
-        return boiteMapper.toDTO(boiteRepository.getReferenceById(id));
+        return boiteRepository.findById(id)
+                .map(boiteMapper::toDTO)
+
+
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Boite not found"));
     }
 
     public BoiteDto createBoite(BoiteDto boiteDTO) {
@@ -37,19 +41,29 @@ public class BoiteService {
     }
 
     public BoiteDto updateBoite(BoiteDto boiteDTO) {
-        Boite existingBoite = boiteRepository.getReferenceById(boiteMapper.toEntity(boiteDTO).getId());
-        existingBoite.setNom(boiteMapper.toEntity(boiteDTO).getNom());
-        existingBoite.setDescription(boiteMapper.toEntity(boiteDTO).getDescription());
-        existingBoite.setPointGeo(boiteMapper.toEntity(boiteDTO).getPointGeo());
-        existingBoite.setQuantite(boiteMapper.toEntity(boiteDTO).getQuantite());
-        Boite savedBoite = boiteRepository.save(existingBoite);
-        return boiteMapper.toDTO(savedBoite);
+        Boite boite = boiteMapper.toEntity(boiteDTO);
+        return boiteRepository.findById(boite.getId())
+                .map(existingBoite -> {
+                    existingBoite.setNom(boite.getNom());
+                    existingBoite.setDescription(boite.getDescription());
+                    existingBoite.setPointGeo(boite.getPointGeo());
+                    existingBoite.setQuantite(boite.getQuantite());
+                    return boiteMapper.toDTO(boiteRepository.save(existingBoite));
+                })
+
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Boite not found"));
     }
 
-     public void deleteBoite(Integer id) {
-        Boite existingBoite = boiteRepository.getReferenceById(id);
-        boiteRepository.delete(existingBoite);
+    
+
+
+    public void deleteBoite(Integer id) {
+        if (!boiteRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Boite not found");
+
+        }
+        boiteRepository.deleteById(id);
+
+
     }
-
-
 }
