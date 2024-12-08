5890f6588844fb3aad6945ee34d483325bce04e5
rmdnhub <rmdn483@gmail.com>
2024-12-09 00:13:36 +0100

    services

--- frontend/src/app/services/boite.service.ts
+++ frontend/src/app/services/boite.service.ts
@@ -4,14 +4,35 @@ import { Observable } from 'rxjs';
 import { Boite } from '../models/boite/boite.module';
 
 @Injectable({
-  providedIn: 'root', // Indique que ce service est accessible globalement
+  providedIn: 'root',
 })
 export class BoiteService {
-  private apiUrl = 'http://localhost:8080/api/boites/all'; // URL de votre API Spring Boot
+  private apiUrl = 'http://localhost:8080/api/boites/';
 
   constructor(private http: HttpClient) {}
 
+  // Récupérer toutes les boîtes
   getBoites(): Observable<Boite[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<Boite[]>(`${this.apiUrl}all`);
+  }
+
+  // Récupérer une boîte par ID
+  getBoiteById(id: number): Observable<Boite> {
+    return this.http.get<Boite>(`${this.apiUrl}${id}`);
+  }
+
+  // Créer une nouvelle boîte
+  createBoite(boite: Boite): Observable<Boite> {
+    return this.http.post<Boite>(`${this.apiUrl}create`, boite);
+  }
+
+  // Mettre à jour une boîte
+  updateBoite(boite: Boite): Observable<Boite> {
+    return this.http.put<Boite>(`${this.apiUrl}update`, boite);
+  }
+
+  // Supprimer une boîte par ID
+  deleteBoite(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}delete/${id}`);
   }
 }
